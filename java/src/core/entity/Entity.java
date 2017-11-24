06c72259484cc1e10115d88568b081960dd032fa
NiGhTFoX <pkg_demon@mail.ru>
2017-11-25 02:22:40 +0400

    Added UUIDs for Entities

--- java/src/core/entity/Entity.java
+++ java/src/core/entity/Entity.java
@@ -5,7 +5,30 @@ package core.entity;
 */
 
 import java.io.Serializable;
+import java.util.UUID;
 
-public abstract class Entity implements Serializable {
+public class Entity implements Serializable {
+    private long version;
+    private UUID uuid;
 
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Entity(){
+        this.version = 13L;
+        this.uuid = UUID.randomUUID();
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
 }
