f8855ff2b4906381afba582a47ffd18cabcff3e2
NiGhTFoX <pkg_demon@mail.ru>
2017-11-24 22:31:38 +0400

    All properties from main was moved to Router Added Router call to EntryPoint as comment Added reflection for controller and method call

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,23 +2,10 @@
 ** Created by NiGhTFoX on 24.11.2017.
 */
 
-import project.helpers.LocationParser;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
-    public static String DEFAULT_CONTROLLER = "Menu";
-    public static String DEFAULT_ACTION = "init";
-    public static final String CONTROLLER_PREFIX = "Controller";
-
-    public static String CONTROLLER_NAME = null;
-    public static String ACTION_NAME = null;
-    public static Map<String, String> params = new HashMap<>();
-
     public static void main (String args[]){
         /* Location Parser Test */
-        LocationParser lp = new LocationParser("controller/action?id=&id2=test2$=test3");
+        /*LocationParser lp = new LocationParser("controller/action?id=&id2=test2$=test3");
         try {
             lp.parse();
             System.out.println(lp.getLocation());
@@ -27,6 +14,13 @@ public class Main {
             System.out.println(lp.getParams().entrySet());
         }catch (Exception exp){
             System.out.println(exp.getMessage());
-        }
+        }*/
+
+        /*Router router = new Router();
+        try {
+            router.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 }
--- java/src/project/helpers/Router.java
+++ java/src/project/helpers/Router.java
@@ -4,5 +4,69 @@ package project.helpers;
 ** Created by NiGhTFoX on 24.11.2017.
 */
 
+import core.interfaces.IController;
+import project.controller.MenuController;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router {
+    private static final String DEFAULT_CONTROLLER = "menu";
+    private static final String DEFAULT_ACTION = "init";
+    private static final String CONTROLLER_PREFIX = "Controller";
+    private static final String CONTROLLER_FOLDER = "project.controller";
+
+    private String CONTROLLER_NAME;
+    private String ACTION_NAME;
+    private Map<String, String> params;
+
+    public void start(){
+
+        if(this.CONTROLLER_NAME == null){
+            this.CONTROLLER_NAME = prepareControllerName(DEFAULT_CONTROLLER);
+        }
+
+        if(this.ACTION_NAME == null){
+            this.ACTION_NAME = DEFAULT_ACTION;
+        }
+
+        if(this.params == null){
+            this.params = new HashMap<>();
+        }
+
+        IController controller;
+
+        try {
+            controller = getController(CONTROLLER_FOLDER + "." + this.CONTROLLER_NAME + CONTROLLER_PREFIX);
+        }catch (Exception exp){
+            controller = new MenuController();
+        }
+
+        Map<String, String> listOfActions;
+
+        try {
+            listOfActions = callAction(controller, this.ACTION_NAME);
+        } catch (Exception e) {
+            listOfActions = new MenuController().init();
+        }
+    }
+
+    private String prepareControllerName(final String name){
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
+    private IController getController(String controller) throws Exception{
+        Class c = Class.forName(controller);
+        Object obj = c.newInstance();
+
+        return (IController) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, String> callAction(IController controller, String action) throws Exception{
+        Method method = controller.getClass().getMethod(action);
+
+        return (Map<String, String>) method.invoke (controller);
+    }
 }
