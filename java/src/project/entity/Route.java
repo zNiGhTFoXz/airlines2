671e9122fb77addf9fdfbd1639215c3503bd3e12
Александр <nadymorigin@gmail.com>
2017-11-25 00:46:04 +0300

    getter n setter for route

--- java/src/project/entity/Route.java
+++ java/src/project/entity/Route.java
@@ -9,5 +9,22 @@ import core.entity.Entity;
 import java.io.Serializable;
 
 public class Route extends Entity implements Serializable{
+    private String flightFrom;
+    private String flightTo;
 
+    public String getFlightFrom() {
+        return flightFrom;
+    }
+
+    public void setFlightFrom(String flightFrom) {
+        this.flightFrom = flightFrom;
+    }
+
+    public String getFlightTo() {
+        return flightTo;
+    }
+
+    public void setFlightTo(String flightTo) {
+        this.flightTo = flightTo;
+    }
 }
