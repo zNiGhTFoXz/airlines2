5a0ecb824dfd73a7945131a4fe4bccfb89a6777c
Александр <nadymorigin@gmail.com>
2017-11-26 01:45:01 +0300

    save methods update

--- java/src/project/model/FlightModel.java
+++ java/src/project/model/FlightModel.java
@@ -10,14 +10,26 @@ import core.model.Model;
 import project.entity.Flight;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 
 public class FlightModel extends Model implements IModel {
+    private Path path = Paths.get("C://FlightDir/");
     @Override
     public void save(Entity obj) {
         try {
-            FileOutputStream fos = new FileOutputStream("temp.out");
+            String filename = obj.getUuid().toString() + "_";
+            if(!Files.isDirectory(path)) {
+                Files.createDirectory(path);
+            }
+            if(((Flight)obj).getRoute() != null) {
+                filename += ((Flight) obj).getRoute().getUuid().toString();
+            }
+
+            FileOutputStream fos = new FileOutputStream(path + filename +".out");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(obj);
             oos.flush();
@@ -32,7 +44,8 @@ public class FlightModel extends Model implements IModel {
     @Override
     public Entity load() {
         try {
-            FileInputStream fis = new FileInputStream("file.out");
+
+            FileInputStream fis = new FileInputStream("Flight/file.out");
             ObjectInputStream oin = new ObjectInputStream(fis);
             Flight flight = (Flight) oin.readObject();
             System.out.println("version:" + flight.getVersion());
--- java/src/project/model/RouteModel.java
+++ java/src/project/model/RouteModel.java
@@ -13,16 +13,24 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class RouteModel extends Model implements IModel{
+    private Path path = Paths.get("C://RouteDir/");
     @Override
-    public void save() {
+    public void save(Entity obj) {
         try {
-            FileOutputStream fos = new FileOutputStream("temp.out");
+            String filename = obj.getUuid().toString();
+            if(!Files.isDirectory(path)) {
+                Files.createDirectory(path);
+            }
+
+            FileOutputStream fos = new FileOutputStream(path + filename +".out");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            Route route = new Route();
-            oos.writeObject(route);
+            oos.writeObject(obj);
             oos.flush();
             oos.close();
         }
