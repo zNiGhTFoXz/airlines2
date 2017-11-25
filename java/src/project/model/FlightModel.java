73ac9f1f880efab50b3605cf7929802bc230efd0
Александр <nadymorigin@gmail.com>
2017-11-25 23:33:32 +0300

    shit

--- java/src/project/model/FlightModel.java
+++ java/src/project/model/FlightModel.java
@@ -7,17 +7,35 @@ package project.model;
 import core.entity.Entity;
 import core.interfaces.IModel;
 import core.model.Model;
+import project.entity.Flight;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
-public class FlightModel extends Model implements IModel{
+
+public class FlightModel extends Model implements IModel {
     @Override
     public void save() {
+        FileOutputStream fos = new FileOutputStream("temp.out");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        Flight flight = new Flight();
+        oos.writeObject(flight);
+        oos.flush();
+        oos.close();
 
     }
 
     @Override
     public Entity load() {
+
+        FileInputStream fis = new FileInputStream("file.out");
+        ObjectInputStream oin = new ObjectInputStream(fis);
+        Flight flight = (Flight) oin.readObject();
+        System.out.println("version:" + flight.getVersion());
+
         return null;
     }
 
