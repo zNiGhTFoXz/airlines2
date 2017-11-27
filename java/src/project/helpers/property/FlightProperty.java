fbe4b2d524a1605af13f91a425fa04ea556a32cd
NiGhTFoX <pkg_demon@mail.ru>
2017-11-28 00:17:25 +0400

    Added property classes with properties for Flight and Route

---
+++ java/src/project/helpers/property/FlightProperty.java
@@ -0,0 +1,11 @@
+package project.helpers.property;
+
+public class FlightProperty {
+
+    public static final String UUID = "uuid";
+    public static final String NUMBER = "number";
+    public static final String AIRBUS = "airbus";
+    public static final String ROUTE = "route";
+    public static final String TIME_FROM = "timeFrom";
+    public static final String TIME_PATH = "timePath";
+}
---
+++ java/src/project/helpers/property/RouteProperty.java
@@ -0,0 +1,7 @@
+package project.helpers.property;
+
+public class RouteProperty {
+    public static final String UUID = "uuid";
+    public static final String FLIGHT_FROM = "flightFrom";
+    public static final String FLIGHT_TO = "flightTo";
+}
